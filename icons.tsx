d626ea5b1db5fb1d5a479e8d56c59708a9789c7e
vellumgracia-del <vellumgracia@gmail.com>
2025-11-22 20:58:51 +0700

    Create icons.tsx

---
+++ icons.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { 
+  Home, 
+  Search, 
+  Library, 
+  PlusSquare, 
+  Heart, 
+  Play, 
+  Pause, 
+  SkipBack, 
+  SkipForward, 
+  Repeat, 
+  Shuffle, 
+  Mic2, 
+  ListMusic, 
+  Volume2, 
+  Maximize2,
+  Clock,
+  MoreHorizontal,
+  Sparkles,
+  Disc
+} from 'lucide-react';
+
+export const Icons = {
+  Home,
+  Search,
+  Library,
+  PlusSquare,
+  Heart,
+  Play,
+  Pause,
+  SkipBack,
+  SkipForward,
+  Repeat,
+  Shuffle,
+  Mic2,
+  ListMusic,
+  Volume2,
+  Maximize2,
+  Clock,
+  MoreHorizontal,
+  Sparkles,
+  Disc
+};
